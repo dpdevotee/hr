752cd4324e9a675006395fc65bfeaff26e0ff629
ddy2 <ddy2@yandex-team.ru>
2022-01-03 02:44:02 +0300

    Viewsets

--- hr/urls.py
+++ hr/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from organization.urls import router as organization_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(organization_router.urls)),
 ]
--- organization/views.py
+++ organization/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets, serializers
+from . import models
 
-# Create your views here.
+
+class RegionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Region
+        fields = ['region_id', 'region_name']
+
+
+class RegionViewSet(viewsets.ModelViewSet):
+    queryset = models.Region.objects.all()
+    serializer_class = RegionSerializer
+
+
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Country
+        fields = ['country_id', 'country_name', 'region']
+
+
+class CountryViewSet(viewsets.ModelViewSet):
+    queryset = models.Country.objects.all()
+    serializer_class = CountrySerializer
